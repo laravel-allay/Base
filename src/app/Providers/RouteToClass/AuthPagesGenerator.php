ca019f1da1c4ae2f91b90e19fc0d459cb833246e
Zachary Schuessler <zlschuessler@gmail.com>
2016-12-30 13:48:18 -0700

    add check for auth pages before returning the auth page class

--- src/app/Providers/RouteToClass/AuthPagesGenerator.php
+++ src/app/Providers/RouteToClass/AuthPagesGenerator.php
@@ -6,8 +6,15 @@ use Zschuessler\RouteToClass\Generators\GeneratorAbstract;
 
 class AuthPagesGenerator extends GeneratorAbstract
 {
+    public $authCtrlClass = 'Allay\Base\app\Http\Controllers\Auth';
+
     public function generateClassName()
     {
-        return 'layout-auth-simple';
+        $action = $this->getRoute()->getAction();
+
+        if (isset($action['uses'])
+            && true === starts_with($action['uses'], $this->authCtrlClass)) {
+            return 'layout-auth-simple';
+        }
     }
 }
