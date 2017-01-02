465c58deb65d3866f6763de47533bf00bf2d2e7d
Zachary Schuessler <zlschuessler@gmail.com>
2017-01-02 10:49:54 -0700

    exit early when generating auth route classes if is closure

--- src/app/Providers/RouteToClass/AuthPagesGenerator.php
+++ src/app/Providers/RouteToClass/AuthPagesGenerator.php
@@ -6,8 +6,21 @@ use Zschuessler\RouteToClass\Generators\GeneratorAbstract;
 
 class AuthPagesGenerator extends GeneratorAbstract
 {
+    public $authCtrlClass = 'Allay\Base\app\Http\Controllers\Auth';
+
     public function generateClassName()
     {
-        return 'layout-auth-simple';
+        $action = $this->getRoute()->getAction();
+        $uses   = $action['uses'];
+
+        // Exit early if called from a closure route
+        if ($uses instanceof \Closure) {
+            return;
+        }
+
+        if (isset($action['uses'])
+            && true === starts_with($uses, $this->authCtrlClass)) {
+            return 'layout-auth-simple';
+        }
     }
 }
